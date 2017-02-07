414bc92546e02f1298ba5fdb679d4e947587ed89
seventromix <seventromix@yandex.ru>
2017-02-07 18:56:58 +0400

    Замутил камеру

---
+++ Assets/Scripts/RotCam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotCam : MonoBehaviour {
+    public float x;
+    public float y;
+    public float z;
+    public float w;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, 1f, Space.World);
+    }
+}
