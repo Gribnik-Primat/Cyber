8e8a6490662e5cfc4be844d619954ce4ebe07763
seventromix <seventromix@yandex.ru>
2017-03-27 21:46:39 +0400

    Гришан ГЛЯНЬ скрипты Uses

---
+++ Assets/Scripts/OpenDoorLock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorLock : MonoBehaviour {
+    public GameObject door;
+    public bool open = false;
+
+    public float doorOpenAngleX;
+    public float doorOpenAngleY;
+    public float doorOpenAngleZ;
+
+    public float doorCloseAngleX;
+    public float doorCloseAngleY;
+    public float doorCloseAngleZ;
+
+    public float smooth = 2f;
+
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        
+            if (open) //open == true
+            {
+                Quaternion targetRotation = Quaternion.Euler(doorOpenAngleX, doorOpenAngleY, doorOpenAngleZ);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+            }
+            else
+            {
+                Quaternion targetRotation2 = Quaternion.Euler(doorCloseAngleX, doorCloseAngleY, doorCloseAngleZ);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smooth * Time.deltaTime);
+            }
+    }
+
+    
+}
+
---
+++ Assets/Scripts/Uses.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uses : MonoBehaviour {
+
+    //  OpenDoorLock door;
+    bool door;
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position + Vector3.up, transform.forward + Vector3.up);
+        if(Physics.Raycast(ray, out hit, 3f))
+        {
+            if (hit.transform.CompareTag("Locker"))
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    door =GetComponent<OpenDoorLock>().open = true;
+                }
+            }
+        }
+
+
+    }
+}
