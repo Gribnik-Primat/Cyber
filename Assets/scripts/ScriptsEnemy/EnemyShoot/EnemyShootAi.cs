30731a3f41453c1a059944cdf3a5e2cfd13c9bca
seventromix <seventromix@yandex.ru>
2017-04-08 20:11:49 +0400

    Стрелок, сталось до делать сам выстрел

--- Assets/scripts/ScriptsEnemy/EnemyShoot/EnemyShootAi.cs
+++ Assets/scripts/ScriptsEnemy/EnemyShoot/EnemyShootAi.cs
@@ -4,51 +4,70 @@ using UnityEngine;
 
 public class EnemyShootAi : MonoBehaviour {
     UnityEngine.AI.NavMeshAgent agent;
-
+    Shoot shoot;
     Animator anim;
     public float visible = 10f; // дальность на сколько видим 
     GameObject Player;
     public float speed = 2f; // speed
     AudioSource sourse;
-    public float angleV = 90f; // угол обозора
+    public float angleV = 70f; // угол обозора
 
-   
-    bool attacking;
+    public float attackRate = 3; // скорость атаки
+    float attackR;
+    public bool attacking;
 
-    
+    public float attackRange = 8;// расстояние до цели
     public float rotSpeed = 5; // скокрость разворота 
 
     public Transform checkpoint;
 
     public GameObject damageCollider; // колайдер дамага
-    bool lookLeft;
 
+    // public bool invisibleplayer;
 
-    Shoot shoot;
+    CharacterStats CharStats;
 
     Transform target;
-   
+    public bool see;
+    float distance;
+
     void Start()
     {
-       
+        see = false;
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         Player = GameObject.FindGameObjectWithTag("Player");
         anim = GetComponent<Animator>();
-        
-        Invoke("move", 10f);       
-    }
+        anim.SetLayerWeight(1, 0f);
+        CharStats = GetComponent<CharacterStats>();
+        Invoke("move", 1f);
+        shoot = GetComponentInChildren<Shoot>();
+        // invisibleplayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Invisibility>().state;
+        // invisibleplayer = false;
 
+        agent.stoppingDistance = attackRange;
+
+    }
     void move()
     {
         CheckPoint point = checkpoint.GetComponent<CheckPoint>();
         checkpoint = point.getNext();
         agent.destination = checkpoint.position;
-        Invoke("move", 10f);
+        Invoke("move", 15f);
     }
+
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, target.position);  // дистанция
+
+        //@@@@@@@@@@@ какого хрена здесь вылетает ошибка NULL REFERENCE Exception?!!!
+        float distance;   // дистанция
+                          //if (invisibleplayer )
+                          //// невидимость игрока
+                          //	distance = visible+1;
+                          //      if (invisibleplayer)
+                          //      {
+
+        distance = Vector3.Distance(transform.position, target.position);
         if (distance < visible)
         {
             Quaternion look = Quaternion.LookRotation(target.transform.position - transform.position);  // угол видимости
@@ -64,72 +83,91 @@ public class EnemyShootAi : MonoBehaviour {
 
                     if (hit.transform.CompareTag("Player"))
                     {
-                        RaycastHit hit1;
-                        Ray ray1 = new Ray(transform.position+Vector3.up, Vector3.forward);
+                        see = true;
 
-
-                        if (Physics.Raycast(ray1, out hit1, visible))          // расстоние меньше то бьем 
+                        if (distance < attackRange)           // расстоние меньше то бьем 
                         {
-                            if (hit1.transform.CompareTag("Player"))
-                            {
-                                attacking = true;
-                            }
+                            anim.SetLayerWeight(1,1f);
+                            attacking = true;
+
                         }
                         else
                         {
+                            anim.SetLayerWeight(1, 0f);
                             attacking = false;
 
                         }
 
 
 
-                        if (!attacking)                     // если не бьем то идем
-                        {
-
+                        if (!attacking)
+                        {                     // если не бьем то идем
+                            anim.SetLayerWeight(1, 0f);
                             agent.Resume();
                             agent.destination = Player.transform.position;
 
 
 
-                            lookLeft = (target.position.z < transform.position.z) ? true : false;       // повороты
+                            //lookLeft = (target.position.z < transform.position.z) ? true : false;       // повороты
 
-                            Quaternion targetRot = transform.rotation;
+                            //Quaternion targetRot = transform.rotation;
 
-                            if (lookLeft)
-                            {
-                                targetRot = Quaternion.Euler(0, 180, 0);
+                            //if (lookLeft)
+                            //{
+                            //    targetRot = Quaternion.Euler(0, 180, 0);
 
-                            }
-                            else
-                            {
-                                targetRot = Quaternion.Euler(0, 0, 0);
-                            }
-                            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotSpeed); // плавность поворота
+                            //}
+                            //else
+                            //{
+                            //    targetRot = Quaternion.Euler(0, 0, 0);
+                            //}
+                            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotSpeed); // плавность поворота
 
                         }
-                        else
+
+                        if (attacking)
                         {
                             agent.Stop();
-                            anim.SetBool("Attack", true);
-                            StartCoroutine("CloseAttack");
-                            shoot.shooting();
+                            attackR += Time.deltaTime;
+
+                            if (attackR > attackRate)                       // атакуем 
+                            {      
+                               anim.SetBool("Attack", true);
+                                shoot.shooting();
+                               StartCoroutine("CloseAttack");
+                               attackR = 0;  
+                            }
+
+
                         }
                     }
                 }
             }
         }
-        if (agent.velocity.magnitude > 0.5)   // запуск анимации
+        // }
+
+        if (agent.velocity.magnitude > 1)   // запуск анимации
         {
             anim.SetBool("Walk", true);
+            anim.SetBool("Run", false);
         }
-        else anim.SetBool("Walk", false);
-       
+        else if (agent.velocity.magnitude > 3)
+        {
+            anim.SetBool("Run", true);
+            anim.SetBool("Walk", false);
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+            anim.SetBool("Run", false);
+        }
     }
 
     IEnumerator CloseAttack()
     {
-       yield return new WaitForSeconds(.4f);
+        yield return new WaitForSeconds(.4f);
         anim.SetBool("Attack", false);
+
     }
     public void OpenDamageCollider()
     {
