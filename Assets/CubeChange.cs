64050554a85f5bbab81a84c6afeb75478434311a
seventromix <seventromix@yandex.ru>
2017-04-08 16:44:50 +0400

    Мини игра гришан глянь скрипты плиииз

---
+++ Assets/CubeChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeChange : MonoBehaviour
+{
+
+    Animator anim;
+    public bool change;
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update ()
+    {
+        if (change)
+        {
+            anim.SetBool("open", true);
+            anim.SetBool("close", false);
+        }
+        if(!change)
+        {
+            anim.SetBool("open", false);
+            anim.SetBool("close", true);
+        }
+
+	}
+}
---
+++ Assets/Minigame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame : MonoBehaviour {
+
+	public Transform point;
+    public float distanse;
+    //CubeChange ch;
+	void Start ()
+    {
+      //  ch = GetComponent<CubeChange>().chage;
+        distanse = 15f;
+	}
+	
+	
+
+	void Update ()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, point.transform.position);
+        Debug.DrawRay(transform.position, point.transform.position);
+        if (Physics.Raycast(ray, out hit, distanse))
+        {
+            if (hit.transform.CompareTag("Key"))
+            {
+        //        ch = !ch;
+            }
+        }
+	}
+}
