279059139ae90240dd7d32079f68c2c292526a64
seventromix <seventromix@yandex.ru>
2017-04-08 17:47:40 +0400

    пабам)

---
+++ Assets/Scripts/oldScript/DoDamage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoDamage : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterStats>())
+        {
+			
+            other.GetComponent<CharacterStats>().checkToApplyDamage();
+        }
+    }
+	
+	
+}
---
+++ Assets/Scripts/oldScript/FollowCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+
+    public Transform target;
+    public float speed;
+    internal static UnityEngine.Camera main;
+
+    void FixedUpdate ()
+    {
+        Vector3 pos = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        transform.position = pos;
+	}
+}
