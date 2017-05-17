78cf0cdace30e6290cc109a43d845957b5f05fc5
grigoriy <gri-bel@yandex.ru>
2017-05-17 21:29:23 +0300

    тутор готов

--- Assets/play_tutorial.cs
+++ Assets/play_tutorial.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class play_tutorial : MonoBehaviour {
 	public AudioClip clip;
 	// Use this for initialization
+	bool disable = false;
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (disable == true)
+			collider.isTrigger = false;
+		else
+			collider.isTrigger = true;
 	}
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
 			AudioSource.PlayClipAtPoint(clip, transform.position);
 		}
+		disable = true;
 	}
 
 }
