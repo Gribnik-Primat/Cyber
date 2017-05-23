0c2c191ae398558a6ee7a59ad8af2c714bb13f1f
seventromix <seventromix@yandex.ru>
2017-05-23 17:46:48 +0400

    враги

---
+++ Assets/ChangeLayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLayer : MonoBehaviour {
+
+    public GameObject player;
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        player.GetComponentInChildren<Animator>().SetLayerWeight(0, 1f);
+        player.GetComponentInChildren<Animator>().SetLayerWeight(1, 0f);
+        Destroy(gameObject);
+	}
+}
