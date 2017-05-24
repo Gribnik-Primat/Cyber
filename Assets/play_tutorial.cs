518cfd9eb69c46e2bec1bb12c440c979899f5663
grigoriy <gri-bel@yandex.ru>
2017-05-24 21:02:25 +0300

    кнопочки :3333

--- Assets/play_tutorial.cs
+++ Assets/play_tutorial.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 [RequireComponent(typeof(AudioSource))]
 
@@ -17,17 +19,24 @@ public class play_tutorial : MonoBehaviour {
 	void Update () {
 		
 	}
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			foreach (GameObject button in buttons)
+				button.gameObject.SetActive(true);
+		}
+	}
 	void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
 			audio.Play();
-
 		}
 	}
 	void OnTriggerExit(Collider other)
 	{
 		if (other.gameObject.tag == "Player") {
-			
+			foreach (GameObject button in buttons)
+				button.gameObject.SetActive(false);
 		}
 	}
 }
