4bc632ebe2314f05217220eb1c64129944b9b9a1
grigoriy <gri-bel@yandex.ru>
2017-05-25 13:07:23 +0300

    готовы триггеры

--- Assets/play_tutorial.cs
+++ Assets/play_tutorial.cs
@@ -10,6 +10,9 @@ public class play_tutorial : MonoBehaviour {
 	public AudioClip clip;
 	AudioSource audio;	// Use this for initialization
 	public GameObject[] buttons;
+	public GameObject[] doors;
+	public float time_for_doors_closed;
+	float time;
 	void Start () {
 		audio = GetComponent<AudioSource>();
 		audio.playOnAwake = false;
@@ -17,10 +20,16 @@ public class play_tutorial : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+			}
 	void OnTriggerStay(Collider other)
 	{
+		time += Time.deltaTime;
+		if (doors.Length > 0) {
+			if (time >= time_for_doors_closed) {
+				foreach (GameObject door in doors)
+					door.gameObject.GetComponent<OpenClose> ().enabled = true;
+			}
+		}
 		if (other.gameObject.tag == "Player") {
 			foreach (GameObject button in buttons)
 				button.gameObject.SetActive(true);
@@ -28,6 +37,10 @@ public class play_tutorial : MonoBehaviour {
 	}
 	void OnTriggerEnter(Collider other)
 	{
+		if (doors.Length > 0) {
+			foreach (GameObject door in doors)
+				door.gameObject.GetComponent<OpenClose> ().enabled = false;
+		}
 		if (other.gameObject.tag == "Player") {
 			audio.Play();
 		}
@@ -38,5 +51,6 @@ public class play_tutorial : MonoBehaviour {
 			foreach (GameObject button in buttons)
 				button.gameObject.SetActive(false);
 		}
+		this.gameObject.SetActive (false);
 	}
 }
