9027f6c825cc392dca3468e2723b8e0f267368b6
seventromix <seventromix@yandex.ru>
2017-05-27 18:40:22 +0400

    после смерти воскрешаемся в одной сцене

---
+++ Assets/Scripts/LoadScence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScence : MonoBehaviour {
+
+    player pl;
+	
+	void Start ()
+    {
+        pl = GameObject.FindGameObjectWithTag("Player").GetComponentInParent<player>();
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            pl.loadS= true;
+            Destroy(gameObject);
+        }
+    }
+}
