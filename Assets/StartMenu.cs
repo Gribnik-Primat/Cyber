03c229326f877369e82e942f42a640aac2747d6d
seventromix <seventromix@yandex.ru>
2017-05-27 15:54:46 +0400

    переходы споледней сцены

---
+++ Assets/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+    float time;
+	public float t;
+	void Start ()
+    {
+        time = 0;
+	}
+	
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if(time > t)
+        {
+            Application.LoadLevel(0);
+            time = 0;
+        }
+	}
+}
