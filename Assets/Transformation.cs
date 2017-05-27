bcc7d6e3474c9c059db7f6c372e89d134925d838
seventromix <seventromix@yandex.ru>
2017-05-27 15:37:08 +0400

    Добавил последию сцену

---
+++ Assets/Transformation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transformation : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        Vector2 toPosition = new Vector2(0,600);
+        transform.localPosition = Vector2.Lerp(transform.localPosition, toPosition, 0.1f * Time.deltaTime);
+
+    }
+}
