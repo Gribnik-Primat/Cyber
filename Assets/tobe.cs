30935ff076e9e5fb544f9815b2a94276d2011155
seventromix <seventromix@yandex.ru>
2017-05-28 17:44:36 +0400

    ещ иу

---
+++ Assets/tobe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tobe : MonoBehaviour {
+
+    public GameObject toBe;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            toBe.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            toBe.SetActive(false);
+        }
+    }
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
