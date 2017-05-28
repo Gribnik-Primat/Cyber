1abe417abb8d3e5006a277e6a66e3c1179d00099
grigoriy <gri-bel@yandex.ru>
2017-05-28 17:32:18 +0300

    To Be Continued

--- Assets/AttackRobots.cs
+++ Assets/AttackRobots.cs
@@ -4,58 +4,61 @@ using UnityEngine;
 using RootMotion.FinalIK;
 using RootMotion.Demos;
 
-public class AttackRobots : MonoBehaviour {
+public class AttackRobots : MonoBehaviour
+{
 
-    public GameObject[] Robots;
-    int n;
-    int r;
-    public CharacterStatsEnemy[] chartRob;
+	public GameObject[] Robots;
+	int n;
+	int r;
+	public CharacterStatsEnemy[] chartRob;
 	bool flag_dead;
 
 	void Start ()
 	{
 		
 		flag_dead = false;
-        r = 0;
+		r = 0;
 		n = 0;
 
 	}
-	
+
 	
 	void Update ()
-    {
-        //n = Random.Range(0, 16);
-    }
-    void OnTriggerStay(Collider other)
-    {
-		for(int i = 0;i<Robots.Length;i++) {
-			chartRob[i] = Robots [i].GetComponent<CharacterStatsEnemy> ();
+	{
+		//n = Random.Range(0, 16);
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		for (int i = 0; i < Robots.Length; i++) {
+			chartRob [i] = Robots [i].GetComponent<CharacterStatsEnemy> ();
 		}
-        if (other.gameObject.tag == "Player")
-        {
-            if (r <= 3)
-            {
+		if (other.gameObject.tag == "Player") {
+			if (r <= 2) {
 				flag_dead = true;
 				for (int i = 0; i < chartRob.Length; i++) {
 					if (chartRob [i].deadrobots == false)
 						flag_dead = false;
 				}
-                if (flag_dead == true)
-                {
-                    r++;
+				if (flag_dead == true) {
+					r++;
 					n += r;
 					for (int i = n - r; i < n; i++) {
 						Robots [i].gameObject.GetComponent<EnemyRobotAI> ().enabled = true;
 						chartRob [i].deadrobots = false;
 					}
                     
-                }
-            }
-            else
-            {
-                Application.LoadLevel(4);
-            }
+				}
+			} else {
+				flag_dead = true;
+				for (int i = 0; i < chartRob.Length; i++) {
+					if (chartRob [i].deadrobots == false)
+						flag_dead = false;
+				}
+				if (flag_dead == true)
+					Application.LoadLevel (4);
+			}
 
-        }
-    }
+		}
+	}
 }
